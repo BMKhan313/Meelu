1a556930333e57a0532e622928ab8551d21c38a4
Hasham Khan <72613380+hashamkhaan@users.noreply.github.com>
2022-12-14 18:20:04 +0500

    Minor

--- src/baseURL/authMultiExport.js
+++ src/baseURL/authMultiExport.js
@@ -1,4 +1,5 @@
 import { useNavigate } from 'react-router-dom';
+// eslint-disable-next-line import/no-extraneous-dependencies
 import Cookies from 'js-cookie';
 // import apiClient from '../services/api';
 import { demoPages } from '../menu';
