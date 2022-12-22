c0893933dcbe5532dc2dbe44c75acba848bb952b
Hasham Khan <72613380+hashamkhaan@users.noreply.github.com>
2022-12-22 17:01:02 +0500

    Logo added

--- src/baseURL/baseURL.js
+++ src/baseURL/baseURL.js
@@ -1,7 +1,7 @@
 // Server
 // const baseURL = 'http://thesfb.live/MeeluBackend/api';
-// const baseURL = 'http://192.168.18.45/MeeluBackend/api'; // Atif
-const baseURL = 'http://192.168.18.20/MeeluBackend/api'; // Auranzeb
+const baseURL = 'http://192.168.18.45/MeeluBackend/api'; // Atif
+// const baseURL = 'http://192.168.18.20/MeeluBackend/api'; // Auranzeb
 
 // const baseURL = 'http://localhost/MeeluBackend/api'; // local
 // const baseURL = 'http://192.168.18.128/MeeluBackend/api'; // local Hashim
